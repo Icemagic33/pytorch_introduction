41f7b8ce89e4c124f80d0dc94bda9ce5a0581fe2
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-05 15:47:22 +0900

    forward

--- pytorch_youtube/AlexNet.py
+++ pytorch_youtube/AlexNet.py
@@ -32,3 +32,10 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
