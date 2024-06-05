3e72d0add9da872eb8eac33a6c0637943634e1ce
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-05 14:59:11 +0900

    LeNet forward pass

--- pytorch_youtube/LeNet.py
+++ pytorch_youtube/LeNet.py
@@ -10,13 +10,46 @@ import torch.nn as nn
 class LeNet(nn.Module):
     def __init__(self, in_channels, num_classes=10):
         super(LeNet, self).__init__()
+        self.relu = nn.ReLU()
         self.c1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(
             5, 5), stride=(1, 1), padding=(0, 0))
         self.s2 = nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
         self.c3 = nn.Conv2d(in_channels=6, out_channels=16,
                             kernel_size=(5, 5), stride=(1, 1), padding=(0, 0))
-        self.s4 = nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.s4 = nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2))  # same as s2
         self.c5 = nn.Conv2d(in_channels=16, out_channels=120,
                             kernel_size=(5, 5), stride=(1, 1), padding=(0, 0))
         self.f6 = nn.Linear(in_features=120, out_features=84)
         self.out = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        x = self.relu(self.c1(x))
+        x = self.s2(x)
+        x = self.relu(self.c3(x))
+        x = self.s4(x)
+        x = self.relu(self.c5(x))  # num_examples x 120 x 1 x 1
+        x = x.reshape(x.shape[0], -1)  # --> num_examples x 120
+        x = self.relu(self.f6(x))
+        x = self.out(x)
+        return x
+
+
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyperparameters
+in_channels = 1
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 5
+
+# Load Data
+train_dataset = datasets.MNIST(
+    root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset,
+                          batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(
+    root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=test_dataset,
+                         batch_size=batch_size, shuffle=True)
