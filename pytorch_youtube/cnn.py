80ce663a3bc46a8bcd629fa893990f3c8293df9e
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-05 13:01:16 +0900

    Add class NN

--- pytorch_youtube/cnn.py
+++ pytorch_youtube/cnn.py
@@ -11,3 +11,21 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 # Transformations we can perform on our dataset
 import torchvision.transforms as transforms
+
+
+# Create Fully Connected Network
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes):  # 28x28 = 784 nodes
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+model = NN(784, 10)
+x = torch.randn(64, 784)
+print(model(x).shape)
