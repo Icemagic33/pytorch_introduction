e1ba5f9de8b337dfb5ae5a274715052e43908cee
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-05 14:35:11 +0900

    class LeNet

--- pytorch_youtube/LeNet.py
+++ pytorch_youtube/LeNet.py
@@ -1,8 +1,13 @@
 # Imports
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+
+
+class LeNet(nn.Module):
+    def __init__(self, in_channels, num_classes=10):
+        super(LeNet, self).__init__()
+        self.c1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=(
+            5, 5), stride=(1, 1), padding=(1, 1))
+        self.s2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.c3 = nn.Conv2d(in_channels=6, out_channels=16,
+                            kernel_size=(5, 5), stride=())
