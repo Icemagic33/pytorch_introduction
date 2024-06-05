76fa1f36418c1104f9db48449d7958a22e3a3b70
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-05 15:46:54 +0900

    self.classifier

--- pytorch_youtube/AlexNet.py
+++ pytorch_youtube/AlexNet.py
@@ -27,4 +27,8 @@ class AlexNet(nn.Module):
             nn.Dropout(p=dropout),
             nn.Linear(256*6*6, 4096),
             nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
         )
