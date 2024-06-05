b316adda6200a097640bb3b6c71c3a8c8da9c27c
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-05 13:32:39 +0900

    train_acc=98.50, test_acc=98.43

--- pytorch_youtube/cnn.py
+++ pytorch_youtube/cnn.py
@@ -13,19 +13,6 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 
-# Create Fully Connected Network
-class NN(nn.Module):
-    def __init__(self, input_size, num_classes):  # 28x28 = 784 nodes
-        super(NN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 50)
-        self.fc2 = nn.Linear(50, num_classes)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-
 # Create Convoluted Neural Network
 class CNN(nn.Module):
     def __init__(self, in_channels=1, num_classes=10):  # if rgb image -> in_channels=3
@@ -89,7 +76,7 @@ in_channels = 1
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
-num_epochs = 1
+num_epochs = 5
 
 # Load Data
 train_dataset = datasets.MNIST(
@@ -143,7 +130,6 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
