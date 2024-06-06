d34ac63c0ded00b63d49dfc2523570aed6c3712b
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-06 16:28:09 +0900

    vgg architecture summary

--- pytorch_youtube/vgg-16.py
+++ pytorch_youtube/vgg-16.py
@@ -7,6 +7,10 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+# VGG16 Architecture summary
+VGG16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
+         'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
 
 class VGG_16(nn.Module):
     def __init__():
