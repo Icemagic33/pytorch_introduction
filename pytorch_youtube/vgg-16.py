18e7e4566e7f265ea1c8e53b69225e4fdc696240
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-06 16:29:07 +0900

    super

--- pytorch_youtube/vgg-16.py
+++ pytorch_youtube/vgg-16.py
@@ -10,8 +10,9 @@ import torchvision.transforms as transforms
 # VGG16 Architecture summary
 VGG16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
          'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+# then, flatten and 4096x4096x1000 linear layers
 
 
 class VGG_16(nn.Module):
-    def __init__():
-        super()
+    def __init__(self, in_channels, num_classes):
+        super(VGG_16, self).__init__()
