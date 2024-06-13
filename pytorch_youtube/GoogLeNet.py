df922cf99b133a8fbe607ab74a318013144c4655
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-13 16:10:28 +0900

    GoogLeNet architecture

--- pytorch_youtube/GoogLeNet.py
+++ pytorch_youtube/GoogLeNet.py
@@ -5,3 +5,5 @@ import torch.nn as nn
 # One big problem with the above modules, at least in this naive form, is that even a modest number of 5x5 convolutions can be prohibitively expensive on top of a convolutional layer with a large number of filters.
 # Inception module with dimension reductions -> have 1x1 conv before 3x3 or 5x5 conv to reduce the dimension that gets passed through larger convolutions.
 # --> 1x1 convolutions are used to compute reductinos before the expensive 3x3 and 5x5 convolutions.
+# conv(7x7/2) -> max pool(3x3/2) -> conv(3x3/1) -> max pool(3x3/2) -> inception(3a) -> inception(3b) -> max pool(3x3/2) -> inception(4a) -> inception(4b) -> inception(4c) -> inception(4d) -> inception(4e) -> max pool(3x3/2) -> inception(5a) -> inception(5b) -> avg pool(7x7/1) -> dropout(40%) -> linear -> softmax
+# Check Table 1 for more details including output size
