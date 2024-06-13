f7e804b943ba5631e8ce9015f4b443a1db47c921
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-14 01:25:07 +0900

    forward pass for self attention

--- pytorch_youtube/transformer.py
+++ pytorch_youtube/transformer.py
@@ -17,3 +17,18 @@ class SelfAttention(nn.Module):
         self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
         self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
         self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads*self.head_dim, embed_size)
+
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        # Split embedding into self.heads pieces
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = keys.reshape(N, key_len, self.heads, self.head_dim)
+
+        energy = torch.einsum()
+        # queries shape: (N, query_len, heads, heads_dim)
+        # keys shape: (N, key_len, heads, heads_dim)
+        # we want energe shpae: (N, heads, query_len, key_len)
