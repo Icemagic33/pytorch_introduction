c537c7f1a68f2317613d5f987c836be3d7a4ced6
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-13 16:07:17 +0900

    GoogLeNet key points update

--- pytorch_youtube/GoogLeNet.py
+++ pytorch_youtube/GoogLeNet.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
 
-# Inception architecture are restriced to filter size 1x1, 3x3, 5x5, however this decision was based more on convenience
+# Inception architecture are restriced to filter size 1x1, 3x3, 5x5, however this decision was based more on convenience rather than necessity
 # One big problem with the above modules, at least in this naive form, is that even a modest number of 5x5 convolutions can be prohibitively expensive on top of a convolutional layer with a large number of filters.
+# Inception module with dimension reductions -> have 1x1 conv before 3x3 or 5x5 conv to reduce the dimension that gets passed through larger convolutions.
+# --> 1x1 convolutions are used to compute reductinos before the expensive 3x3 and 5x5 convolutions.
