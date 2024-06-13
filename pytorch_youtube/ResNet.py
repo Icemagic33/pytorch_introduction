98ea5ddc26decafb719e774f7b9221351ac0814a
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-13 17:35:18 +0900

    forward pass for each block. identity_downsample

--- pytorch_youtube/ResNet.py
+++ pytorch_youtube/ResNet.py
@@ -23,3 +23,21 @@ class block(nn.Module):
         self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
         self.relu = nn.ReLU()
         self.identity_downsample = identity_downsample
+
+    def forward(self, x):
+        identity = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
