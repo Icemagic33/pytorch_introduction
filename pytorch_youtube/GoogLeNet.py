073fe002312015baeeda5fc57666eae5a9e19302
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-13 16:05:07 +0900

    GoogLeNet key points

--- pytorch_youtube/GoogLeNet.py
+++ pytorch_youtube/GoogLeNet.py
@@ -1,2 +1,5 @@
 import torch
 import torch.nn as nn
+
+# Inception architecture are restriced to filter size 1x1, 3x3, 5x5, however this decision was based more on convenience
+# One big problem with the above modules, at least in this naive form, is that even a modest number of 5x5 convolutions can be prohibitively expensive on top of a convolutional layer with a large number of filters.
