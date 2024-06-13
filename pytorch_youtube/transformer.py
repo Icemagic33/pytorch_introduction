f5ab10cc72af92480f29049a287f5df2f8080991
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-14 01:20:52 +0900

    Create class SelfAttention

--- pytorch_youtube/transformer.py
+++ pytorch_youtube/transformer.py
@@ -1,2 +1,19 @@
 import torch
 import torch.nn as nn
+
+
+# Self Attention
+class SelfAttention(nn.Module):
+    # heads : how many parts we are going to split the embed_size into
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert (self.head_dim * heads ==
+                embed_size), "Embed size needs to be divisible by heads"
+
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
