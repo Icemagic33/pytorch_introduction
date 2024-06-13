7d33fe9241ff93e9ac7af7167b721269b4850ba3
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-13 17:05:22 +0900

    Import torch

---
+++ pytorch_youtube/ResNet.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+# 1st place in ILSVRC 2015 classification task
+# Problem: Adding more layers to a suitably deep model leads to higher training error
+
+# 34-layer residual
