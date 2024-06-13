4a9edadf775a06ddc0a84a5229311dc104acea63
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-13 16:11:52 +0900

    auxiliary classifiers explain

--- pytorch_youtube/GoogLeNet.py
+++ pytorch_youtube/GoogLeNet.py
@@ -7,3 +7,4 @@ import torch.nn as nn
 # --> 1x1 convolutions are used to compute reductinos before the expensive 3x3 and 5x5 convolutions.
 # conv(7x7/2) -> max pool(3x3/2) -> conv(3x3/1) -> max pool(3x3/2) -> inception(3a) -> inception(3b) -> max pool(3x3/2) -> inception(4a) -> inception(4b) -> inception(4c) -> inception(4d) -> inception(4e) -> max pool(3x3/2) -> inception(5a) -> inception(5b) -> avg pool(7x7/1) -> dropout(40%) -> linear -> softmax
 # Check Table 1 for more details including output size
+# Auxiliary classifiers are used only during training
