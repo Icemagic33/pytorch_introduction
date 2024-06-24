44d4c88a69b544969d57800e9e811edb5f46302a
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-24 19:20:29 +0900

    Update ViT.py

--- pytorch_youtube/aladdin-persson/ViT.py
+++ pytorch_youtube/aladdin-persson/ViT.py
@@ -13,7 +13,7 @@ class Compose(object):
 	def __init__(self, transforms):
 		self.transforms = transforms
 
-	def __call__(self, image, target);
+	def __call__(self, image, target):
 		for t in self.transforms:
 			image = t(image)
 		return image, target
