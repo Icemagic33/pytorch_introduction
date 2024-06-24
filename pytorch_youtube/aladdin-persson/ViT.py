5bf3931c0e8038e28d72d9857bd03d5468b2911d
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-24 19:01:39 +0900

    Create Compose class

--- pytorch_youtube/aladdin-persson/ViT.py
+++ pytorch_youtube/aladdin-persson/ViT.py
@@ -1,1 +1,19 @@
 # Vision transformer
+
+import torch
+from torchvision.datasets import OxfordIIITPet
+import matplotlib.pyplot as plt
+from random import random
+from torchvision.transforms import Resize, ToTensor
+from torchvision.transforms.functional import to_pil_image
+
+to_tensor = [Resize((144,144)), ToTensor()]
+
+class Compose(object):
+	def __init__(self, transforms):
+		self.transforms = transforms
+
+	def __call__(self, image, target);
+		for t in self.transforms:
+			image = t(image)
+		return image, target
