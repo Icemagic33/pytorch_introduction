ebff9490c31c8a4768067be3caec097d96167fbd
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-28 23:07:07 +0900

    Create Jupyter Notebook

--- pytorch_youtube/aladdin-persson/.ipynb_checkpoints/yolov1-checkpoint.py
+++ pytorch_youtube/aladdin-persson/.ipynb_checkpoints/yolov1-checkpoint.py
@@ -2,5 +2,3 @@
 # Each bounding box for each cell will have: [x, y, w, h]
 # x, y are between 0 to 1 (boudning boxes have top left corner (0,0) and bottom right corner (1,1))
 # w, h can be greater than 1 if the object is wider or taller than the cell
-import torch
-import torch.nn as nn
