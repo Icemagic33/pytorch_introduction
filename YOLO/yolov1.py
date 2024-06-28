953d0ccb1c977b35ff02079831f19bb4f5628b7a
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-28 23:34:23 +0900

    Create CNN Block

--- YOLO/yolov1.py
+++ YOLO/yolov1.py
@@ -16,12 +16,14 @@ architecture_config = [
     (1, 256, 1, 0),
     (3, 512, 1, 1),
     "M",
-    # List:
-    [(1, 512, 1, 0), (3, 512, 1, 1), 4],
+    # List: tuples, and then last integer represents number of repeats
+    [(1, 512, 1, 0), (3, 512, 1, 1), 4],  # last value(=4)
+    # is how many times these sequences should be repeated
     (1, 512, 1, 0),
     (3, 1024, 1, 1),
     "M",
-    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],  # last value(=2)
+    # is how many times these sequences should be repeated
     (3, 1024, 1, 1),
     (3, 1024, 2, 1),
     (3, 1024, 1, 1),
@@ -29,13 +31,17 @@ architecture_config = [
 ]
 
 
-class YOLO(nn.Module):
-    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
-        super(YOLO, self).__init__()
-        self.layer1 = nn.Sequential{
-            nn.Conv2d(in_channels=, out_channels=, kernel_size=(7, 7), stride=, padding=)
-
-        }
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        # conv layer -> batch norm (not in paper, but helps performance) -> leaky relu
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
 
     def forward(self, x):
-        x = nn.ReLU(x.first)
+        return self.leakyrelu(self.batchnorm(self.conv(x)))
+
+
+class Yolov1(nn.Module):
+    def __init__(self, in_channels=3, **kwargs)  # default 3 for RGB image
