4c77925045b9b29d797bc41b92b10a312b5d2dba
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-06-28 23:29:29 +0900

    architecture_config

--- YOLO/yolov1.py
+++ YOLO/yolov1.py
@@ -5,6 +5,29 @@
 import torch
 import torch.nn as nn
 
+architecture_config = [
+    # Tuple:  kernel_size, # of filters = output depth, stride, padding
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    # List:
+    [(1, 512, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
 
 class YOLO(nn.Module):
     def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
