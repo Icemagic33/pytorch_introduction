73ec011984f88e5fcda75d3312c35568d3b622ac
Adam Roberge <78094725+Icemagic33@users.noreply.github.com>
2024-07-02 09:55:28 +0900

    Visualize the counts for each diagnostic severity

--- U-Net/unet_clf/data_vis.py
+++ U-Net/unet_clf/data_vis.py
@@ -15,10 +15,31 @@ import warnings
 # Access to GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 # File location
 fd = "/content/drive/MyDrive/rsna-2024-lumbar-spine-degenerative-classification"
 train = pd.read_csv(f'{fd}/train.csv')
 
+
 # Check how the training files are organized
 print("Total Cases: ", len(train))
 print(train.columns)
+
+
+# Visualize the distribution of different diagnostic categories
+figure, axis = plt.subplots(1, 3, figsize=(20, 5))
+
+# three main diagnostic categories
+for idx, d in enumerate(['foraminal', 'subarticular', 'canal']):
+    # inlucde only the filters that are related to the current diagnostic category
+    diagnosis = list(filter(lambda x: x.find(d) > -1, train.columns))
+
+    dff = train[diagnosis]
+    with warnings.catch_warnings():
+        # calculate the frequency of each diagnostic severity for each condition within the current category
+        # and transpose the result for plotting
+        warnings.simplefilter(action='ignore', category=FutureWarning)
+        value_counts = dff.apply(pd.value_counts).fillna(0).T
+    # plot the value counts as a stacked bar chart on the corresponding subplot
+    value_counts.plot(kind='bar', stacked=True, ax=axis[idx])
+    axis[idx].set_title(f'{d} distribution')
